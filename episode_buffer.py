fb4532ec83534617ce243e90f3fb5a6d4d88c3a4
Torstein Fossnes <torsteinhf@gmail.com>
2026-04-11 15:31:56 +0200

    impl episode buffer + created a hard coded alt for 2048 outside of gameinterface

--- episode_buffer.py
+++ episode_buffer.py
@@ -1,18 +1,32 @@
 
-# episode buffer
+import random
+from config import ROLL_AHEAD, BUFFER_SIZE
+
 class EpisodeBuffer():
 
-    def __init__(self) -> None:
+    def __init__(self, max_size: int = BUFFER_SIZE) -> None:
         self.episodes = []
+        self.max_size = self.max_size
 
-    # anticipating list of 5 tuples
     def add_episode(self, episode):
-        self.episodes.append(episode)
+        if episode:
+            self.episodes.append(episode)
+        if len(self.episodes) > self.max_size:
+            self.episodes.pop(0)
 
-    def get_portion_of_ep(self, mbs, num = -1):
-        ep = self.episodes[num]
-        if mbs > len(ep): 
-            mbs = len(ep)
-        train = ep[0:mbs][:2]
-        target = ep[num][0:mbs][2:]
-        return train, target
+    def sample(self, batch_size: int) -> list:
+        batch = []
+        for _ in range(batch_size):
+            ep = random.choice(self.episodes) # select ep
+            k = random.randint(0, len(ep) - 1) # select obs in ep
+            batch.append({
+                "state_seq": ep[k]["state_seq"],
+                "actions":  [ep[k + t]["action"] for t in range(ROLL_AHEAD)],
+                "policies": [ep[k + t]["action"] for t in range(ROLL_AHEAD + 1)],
+                "values":   [ep[k + t]["action"] for t in range(ROLL_AHEAD + 1)],
+                "rewards":  [ep[k + t]["action"] for t in range(ROLL_AHEAD)],
+            })
+        return batch
+    
+    def __len__(self):
+        return len(self.episodes)
